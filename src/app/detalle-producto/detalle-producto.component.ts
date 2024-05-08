fedfe0c52ea61d515bb7a7db6042c29b0f0fac01
Walter-Lauxmann <walterlauxmann@institutocedec.com>
2024-05-08 14:59:31 -0300

    02 - Agregando navegación

---
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Producto, productos } from '../productos';
+
+@Component({
+  selector: 'app-detalle-producto',
+  templateUrl: './detalle-producto.component.html',
+  styleUrl: './detalle-producto.component.css'
+})
+export class DetalleProductoComponent implements OnInit {
+
+  producto: Producto | undefined;
+
+  constructor(private ruta: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Obtener el id que viene en la ruta
+    const rutaParam = this.ruta.snapshot.paramMap;
+    const productoId = Number(rutaParam.get('id'));
+
+    // Buscar el producto con el id de productoId
+    this.producto = productos.find(producto => producto.id === productoId);
+  }
+
+}
